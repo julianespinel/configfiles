cfe29d9cef5f45616713d8a83800f93580c2df06
Julian Espinel <julianespinel@gmail.com>
2016-03-25 12:19:34 -0500

    Add scripts to ease the spark cluster management.

---
+++ aws/spark/login-to-cluster.sh
@@ -0,0 +1,1 @@
+./spark-ec2 -k spark-keypair -i aws-credentials/spark-keypair.pem login challenge-cluster
---
+++ aws/spark/start-cluster.sh
@@ -0,0 +1,1 @@
+./spark-ec2 --key-pair=spark-keypair --identity-file=aws-credentials/spark-keypair.pem launch challenge-cluster
