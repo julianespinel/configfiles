0c9e597c8acf604d6437489086d29ddfa533ace9
Julian Espinel <julianespinel@gmail.com>
2016-03-25 19:26:20 -0500

    Add more scripts to manage the spark cluster.

---
+++ aws/spark/submit-job.sh
@@ -0,0 +1,2 @@
+# Changle cluster address
+./spark-submit --master spark://ec2-23-23-16-155.compute-1.amazonaws.com:7077 --driver-library-path ~/RedshiftJDBC41-1.1.13.1013.jar --driver-class-path ~/RedshiftJDBC41-1.1.13.1013.jar ~/fromus/withspark.py
