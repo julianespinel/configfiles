6f37d10fc8207d44652d3df163461f64ff5a238d
Julian Espinel <julianespinel@gmail.com>
2016-03-29 09:36:59 -0500

    Improve create-cluster script.

--- aws/spark/create-cluster.sh
+++ aws/spark/create-cluster.sh
@@ -1,1 +1,1 @@
-./spark-ec2 -k spark-keypair -i aws-credentials/spark-keypair.pem -s 8 launch challenge-cluster
+./spark-ec2 -k spark-keypair -i aws-credentials/spark-keypair.pem -s 3 --hadoop-major-version=yarn launch challenge-cluster
