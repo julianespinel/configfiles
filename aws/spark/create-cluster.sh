3c3d66b398e52e0ed2fcd242d64b6ea12143cef4
Julian Espinel <julianespinel@gmail.com>
2016-04-01 10:33:40 -0500

    Update hadoop version.

--- aws/spark/create-cluster.sh
+++ aws/spark/create-cluster.sh
@@ -1,1 +1,1 @@
-./spark-ec2 -k spark-keypair -i aws-credentials/spark-keypair.pem -s 3 --hadoop-major-version=yarn launch challenge-cluster
+./spark-ec2 -k spark-keypair -i aws-credentials/spark-keypair.pem -s 3 --hadoop-major-version=2 launch challenge-cluster
