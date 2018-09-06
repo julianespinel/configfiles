61e2c442cc2a4b8c95ceaef49a0cb2b97ff0bdad
Julian Espinel <julianespinel@gmail.com>
2018-09-06 16:52:58 -0500

    Echo message to not forget to install plugins.

--- tmux/config_tmux.sh
+++ tmux/config_tmux.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp ~/.tmux.conf ~/.tmux.conf.bak
 cp .tmux.conf ~/
+echo "***** Do not forget to start tmux and run `prefix + I` to install plugins. *****"
