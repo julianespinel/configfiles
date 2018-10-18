b77548a1f6c8e94574837749145c09aa7e08990a
Julian Espinel <julianespinel@gmail.com>
2018-10-18 16:06:41 -0500

    Add line to fix keyboard layout mappings.

--- linux/configure-mac-keyboard.sh
+++ linux/configure-mac-keyboard.sh
@@ -1,2 +1,3 @@
 echo options hid_apple swap_opt_cmd=1 | sudo tee -a /etc/modprobe.d/hid_apple.conf
+echo options hid_apple iso_layout=0 | sudo tee -a /etc/modprobe.d/hid_apple.conf
 sudo update-initramfs -u -k all
