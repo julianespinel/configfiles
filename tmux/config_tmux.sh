4a2c9078f27b6539a2798eaa9f5e08b9e5a5a20b
Julian Espinel <julianespinel@gmail.com>
2018-10-18 16:06:14 -0500

    Install xsel to make tmux-yank work.

--- tmux/config_tmux.sh
+++ tmux/config_tmux.sh
@@ -1,3 +1,6 @@
+# Install xsel to make tmux-yank work.
+sudo apt-get install xsel
+# Install tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp ~/.tmux.conf ~/.tmux.conf.bak
 cp .tmux.conf ~/
