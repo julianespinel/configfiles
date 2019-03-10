0875e91a6735fe669ed2a464cf35b602c776c89b
Julian Espinel <julianespinel@gmail.com>
2019-03-09 19:51:44 -0500

    Enable modes.
    1. Evil
    1. Markdown
    1. Company
    1. Flycheck
    1. Magit

--- emacs/init.el
+++ emacs/init.el
@@ -16,6 +16,12 @@
 
 ;;; turn on bracket match highlight
 (show-paren-mode 1)
+
+;;; Evil mode
+(package-install 'evil)
+(require 'evil)
+(evil-mode 1)
+
 ;;; monokai
 (package-install 'monokai-theme)
 (load-theme 'monokai t)
@@ -46,3 +52,30 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;;; Markdown mode
+(package-install 'markdown-mode)
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(autoload 'gfm-mode "markdown-mode"
+   "Major mode for editing GitHub Flavored Markdown files" t)
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+
+
+;;; company-mode
+(package-install 'company)
+(add-hook 'after-init-hook 'global-company-mode)
+(setq company-idle-delay 0) ; No delay in showing suggestions.
+(setq company-minimum-prefix-length 1) ; Show suggestions after entering one character.
+
+
+;;; Flycheck
+(package-install 'flycheck)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+
+;;; magit
+(package-install 'magit)
