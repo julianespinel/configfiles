366f8785a501c0e36f73ee71bc5ce02909baca1f
Julian Espinel <julianespinel@gmail.com>
2019-04-26 21:27:15 +0300

    Improve echo message format.

--- tmux/config_tmux.sh
+++ tmux/config_tmux.sh
@@ -4,4 +4,4 @@ sudo apt-get install xsel
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp ~/.tmux.conf ~/.tmux.conf.bak
 cp .tmux.conf ~/
-echo "***** Do not forget to start tmux and run `prefix + I` to install plugins. *****"
+echo "***** Do not forget to start tmux and run 'prefix + I' to install plugins. *****"
