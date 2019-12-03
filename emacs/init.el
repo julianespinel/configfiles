21eb95256c295625ecb12176afbe5ca9a2e14a6c
Julian Espinel <julianespinel@gmail.com>
2019-12-03 20:25:36 +0200

    Execute stylish-haskell on save

--- emacs/init.el
+++ emacs/init.el
@@ -53,6 +53,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(haskell-stylish-on-save t)
  '(package-selected-packages
    (quote
     (evil-comentary ace-window evil magit flycheck company markdown-mode neotree powerline all-the-icons monokai-theme))))
