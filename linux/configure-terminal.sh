0e5def6e79b993ebfba85eee265691f56e549cea
Julian Espinel <julianespinel@gmail.com>
2020-01-10 21:05:22 +0200

    Add .bash_aliases to script

--- linux/configure-terminal.sh
+++ linux/configure-terminal.sh
@@ -1,7 +1,9 @@
 # Backup existing files.
+cp ~/.bash_aliases ~/.bash_aliases.bak
 cp ~/.bash_profile ~/.bash_profile.bak
 cp ~/.bashrc ~/.bashrc.bak
 
 # Copy bash files from this repo to home.
+cp .bash_aliases ~/
 cp .bash_profile ~/
 cp .bashrc ~/
